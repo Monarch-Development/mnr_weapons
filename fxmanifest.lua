ea449c1c4aee43c656107adcfdaa6280855644e2
IlMelons <melonss2002@gmail.com>
2025-07-17 18:46:17 +0200

    Added fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+name "mnr_weapons"
+description "Weapon management system for servers"
+author "IlMelons"
+version "1.0.0"
+repository "https://github.com/Monarch-Development/mnr_weapons"
+
+files {
+    "meta/*.lua",
+}
+
+data_file "WEAPON_METADATA_FILE" "meta/*.lua"

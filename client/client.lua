fb79f952cc91e2ff11d20d5bea3fe8868aae2ea1
IlMelons <melonss2002@gmail.com>
2025-07-22 15:18:38 +0200

    Added NPCs pickups disabler

--- client/client.lua
+++ client/client.lua
@@ -3,3 +3,9 @@ local pickups = lib.load("config.pickups")
 for hash, toggle in pairs(pickups) do
     ToggleUsePickupsForPlayer(cache.playerId, hash, toggle)
 end
+
+AddEventHandler("populationPedCreating", function(x, y, z)
+    Wait(1000)
+    local _, npc = GetClosestPed(x, y, z, 1.0)
+    SetPedDropsWeaponsWhenDead(npc, false)
+end)
